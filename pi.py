c0e6316cfd4cb979c63b2b192e968601f807ad88
Yuka0830 <y.namiki.24.8.1576@4nonome.com>
2024-09-04 21:18:11 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -3,5 +3,5 @@ text = """
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-# TODO
-
+numbers=list(map(len, ["How I want a drink","alcoholic of course", "after the heavy chapters involving quantum mechanics", "All of thy geometry","Herr Planck","is fairly hard"]))
+print(numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5])
