e6ac543df286bf7bbf70cbf750f174665c974e78
Yuka0830 <y.namiki.24.8.1576@4nonome.com>
2024-09-05 10:25:21 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -3,5 +3,6 @@ text = """
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-numbers=list(map(len, ["How I want a drink","alcoholic of course", "after the heavy chapters involving quantum mechanics", "All of thy geometry","Herr Planck","is fairly hard"]))
-print(numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5])
+words=text.replace(",","").replace(".","").split()
+length=list(map(len,words))
+print(''.join(map(str,length)))
