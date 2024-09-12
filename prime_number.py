69350ad670049dd224fce579540e81515d50d3b8
Yuka0830 <y.namiki.24.8.1576@4nonome.com>
2024-09-12 19:09:43 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,21 +1,20 @@
-#aの値を入力、と表示されたら、今回は61と入力してください
 a = int(input("aの値を入力: "))
-i=2
-while i<a:
-    if a%i==0:
-        print("素数でない")
-        break
-    i+=1
-if i>=a:
-    print("素数である")
-
-#bの値を入力、と表示されたら、今回は10と入力してください
 b = int(input("bの値を入力: "))
-i=2
-while i<b:
-    if b%i==0:
-        print("素数でない")
-        break
-    i+=1
-if i>=b:
-    print("素数である")
+
+def prime(n):
+ if type(n)!=int or n<1:
+   return "error"
+ elif n==1:
+   return "False"
+ elif n<=3:
+   return "True"
+ elif n%2==0:
+   return "False"
+ else:
+   for i in range(3, int(n**0.5)+1,2):
+        if n%i==0:
+          return "False"
+ return "True"
+        
+print(prime(a))
+print(prime(b))
