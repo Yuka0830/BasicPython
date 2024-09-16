619b8447ab2115fa5b13ddee946b7f809ec8f767
Yuka0830 <y.namiki.24.8.1576@4nonome.com>
2024-09-16 14:47:50 +0900

    関数の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,10 +1,15 @@
-from math import sin,pi
-a=0
-b=pi / 2
-N=100
-h=(b-a) / N
-S=0
-for i in range(N):
-    S+=sin(a+i*h)+sin(a+(i+1)*h)
-S*=h/2
-print(S)
+from math import sin,pi,exp,sqrt
+def integral(f,a=0,b=1,n=100):
+  h=(b-a)/n
+  S=0
+  for i in range(n):
+    S+=(f(a+i*h)+f(a+(i+1)*h))
+  S*=(h/2)
+  return S
+print(integral(sin,0,pi/2,50))
+def f1(x):
+  return 4/(1+x**2)
+print(integral(f1,0,1,100))
+def f2(x):
+  return sqrt(pi)*exp(-x**2)
+print(integral(f2,-100,100,1000))
