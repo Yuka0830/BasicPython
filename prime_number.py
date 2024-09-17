290610c22bbc217d62f46f7921eea1a17659b44b
Yuka0830 <y.namiki.24.8.1576@4nonome.com>
2024-09-17 15:20:03 +0900

    関数の解答

--- prime_number.py
+++ prime_number.py
@@ -2,19 +2,19 @@ a = int(input("aの値を入力: "))
 b = int(input("bの値を入力: "))
 
 def prime(n):
- if type(n)!=int or n<1:
+ if isinstance(n, int)==False or n<1:
    return "error"
  elif n==1:
-   return "False"
+   return False
  elif n<=3:
-   return "True"
+   return True
  elif n%2==0:
-   return "False"
+   return False
  else:
    for i in range(3, int(n**0.5)+1,2):
         if n%i==0:
-          return "False"
- return "True"
+          return False
+ return True
         
 print(prime(a))
 print(prime(b))
