8ed0ce2606987dc5f647e1ccd920100ea512f098
Yuka0830 <y.namiki.24.8.1576@4nonome.com>
2024-09-20 19:00:02 +0900

    関数の解答

--- prime_number.py
+++ prime_number.py
@@ -1,22 +1,2 @@
-a = int(input("aの値を入力: "))
-b = int(input("bの値を入力: "))
 
-def prime(n):
- if not isinstance(n, int):
-   raise TypeError("整数を入力してください")
- if n<1:
-   raise ValueError("1より大きい整数を入力してください")
- elif n==1:
-   return False
- elif n<=3:
-   return True
- elif n%2==0:
-   return False
- else:
-   for i in range(3, int(n**0.5)+1,2):
-        if n%i==0:
-          return False
- return True
-        
-print(prime(a))
-print(prime(b))
+
